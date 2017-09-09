aa144ecabe19d65ca4a77c6fe863b5e58a672fb3
KarthikNageswaran <karthick.information@gmail.com>
2017-09-09 19:06:57 +0530

    commiting

--- src/test/java/com/testingApps/hybrid/test.java
+++ src/test/java/com/testingApps/hybrid/test.java
@@ -1,11 +1,21 @@
 package com.testingApps.hybrid;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class test {
+	WebDriver driver;
+	@BeforeTest
+	public void open_browser(){
+		driver=new FirefoxDriver();
+		driver.manage().window().maximize();
+	}
 	
 	@Test
 	public void sample(){
+		driver.get("http://google.com");
 		System.out.println("Test");
 	}
 }
